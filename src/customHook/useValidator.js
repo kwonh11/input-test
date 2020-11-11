0a05fe0cde17023d30474c436f77862eba8d4f64
kwonh11 <kwonh11@gmail.com>
2020-11-12 02:18:16 +0900

    Refector: effect함수에 dependency 추가

--- src/customHook/useValidator.js
+++ src/customHook/useValidator.js
@@ -14,23 +14,23 @@ export default function useValidator(dependencies) {
     React.useEffect(() => {
         if (emailValue && emailValue.includes("@")) setEmailValidation(true);
         if (!emailValue) setEmailValidation(false);
-    }, [emailValue]);
+    }, [emailValue, setEmailValidation]);
     React.useEffect(() => {
         const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{8,16}$/gm;
         if (reg.test(passwordValue)) setPasswordValidation(true);
         if(!passwordValue) setPasswordValidation(false);
-    }, [passwordValue]);
+    }, [passwordValue, setPasswordValidation]);
     React.useEffect(() => {
         if (companyValue) setCompanyValidation(true);
         if (!companyValue) setCompanyValidation(false);
-    }, [companyValue]);
+    }, [companyValue, setCompanyValidation]);
     React.useEffect(() => {
         if (currencyValue) setCurrencyValidation(true);
         if (!currencyValue) setCurrencyValidation(false);
-    }, [currencyValue]);
+    }, [currencyValue, setCurrencyValidation]);
     React.useEffect(() => {
         if (establishedValue) setEstablishedValidation(true);
-    }, [establishedValue]);
+    }, [establishedValue, setEstablishedValidation]);
     React.useEffect(() => {
         if (emailValidation &&
             passwordValidation &&
@@ -41,5 +41,5 @@ export default function useValidator(dependencies) {
             } else {
                 setSendable(false);
             }
-    }, [emailValidation, passwordValidation, companyValidation, currencyValidation, establishedValidation]);
+    }, [emailValidation, passwordValidation, companyValidation, currencyValidation, establishedValidation, setSendable]);
 };
